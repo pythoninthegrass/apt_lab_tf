6a1d1ae8bb58b33bacc3616fb817510bdccc6b53
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2023-08-27 15:58:22 -0500

    More error handling, absolute paths, cleanup and trap functions

--- master/modules/linux/bootstrap.sh
+++ master/modules/linux/bootstrap.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/env bash
 
+# shellcheck disable=SC2086,SC2164,SC2317
+
 set -euo pipefail
 
+script_dir=$(cd "$(dirname "$0")" && pwd)
+
+# remove files/dirs
+clean_up() {
+	printf "Removing %s\n" "$@"
+	rm -rf "$@"
+}
+
+# call function on EXIT SIGINT SIGTERM
+trap_card() {
+	echo "Caught EXIT/SIGINT/SIGTERM."
+	cd "${script_dir}"
+	clean_up /opt/linuxtools.7z
+	clean_up /opt/{impacket,CrackMapExec,helk,john-*,Responder,SilentTrinity}
+}
+trap trap_card EXIT SIGINT SIGTERM
+
 # install python3.11
-apt update && apt install -y software-properties-common
-add-apt-repository -y ppa:deadsnakes/ppa
-apt update && apt install -y python3.11 python3.11-venv p7zip-full tree
+sudo apt update && sudo apt install -y software-properties-common
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt update && sudo apt install -y \
+	ansible ansible-lint curl git python3.11 python3.11-venv p7zip-full tree
 
 # set path for pip
-logged_in_user=$(logname)
+[[ -n $(logname >/dev/null 2>&1) ]] && logged_in_user=$(logname) || logged_in_user=$(whoami)
 logged_in_home=$(eval echo "~${logged_in_user}")
 export PATH=${PATH//":${logged_in_home}/.local/bin"/}
 
@@ -16,23 +36,22 @@ export PATH=${PATH//":${logged_in_home}/.local/bin"/}
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.11
 
 # install tools
-chmod 777 /opt
-cd /opt/
-wget https://github.com/pythoninthegrass/apt_lab_tf_linux/raw/master/linuxtools.7z
-7z x /opt/linuxtools.7z
-mkdir -p /opt/SilentTrinity
-mkdir -p /opt/CrackMapExec
+sudo chmod 777 /opt
+curl -LJ https://github.com/pythoninthegrass/apt_lab_tf_linux/raw/master/linuxtools.7z \
+	-o /opt/linuxtools.7z
+7z x -y /opt/linuxtools.7z -o/opt
+mkdir -p /opt/{CrackMapExec,SilentTrinity}
 mv /opt/st /opt/SilentTrinity/
 mv /opt/cme* /opt/CrackMapExec/
-rm /opt/linuxtools.7z
 git clone https://github.com/lgandx/Responder.git /opt/Responder
 
 # install impacket and setup venv
 git clone https://github.com/SecureAuthCorp/impacket.git /opt/impacket
-cd /opt/impacket
-python3.11 -m venv env
+python3.11 -m venv /opt/impacket/env
 
+# TODO: run on x86_64 vm w/5gb ram
 # clone and run helk container
-git clone https://github.com/Cyb3rWard0g/HELK.git /opt/HELK
-cd /opt/HELK/docker
-./helk_install.sh -p hunting -i 10.10.98.20 -b 'helk-kibana-analysis-alert'
+git clone https://github.com/Cyb3rWard0g/HELK.git /opt/helk
+sudo /opt/helk/docker/helk_install.sh -p hunting -i 10.10.98.20 -b 'helk-kibana-analysis-alert'
+
+exit 0
